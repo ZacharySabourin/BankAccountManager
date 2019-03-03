1f1809e1f4b8a2d9c53445e67e94b02954c05177
Zachary Sabourin <45858290+ZacharySabourin@users.noreply.github.com>
2019-03-03 11:48:27 -0500

    Update Assign1.java

--- Assign1.java
+++ Assign1.java
@@ -1,11 +1,3 @@
-/*
- * Name: Zachary Sabourin
- * Student ID: 040940295
- * Course & Section: CST8132 311
- * Assignment: Assingment 1
- * Date: February 17, 2019
- */
-
 //Imported package to allow user input.
 import java.util.Scanner;
 
